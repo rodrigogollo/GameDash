556f9b5ff6d404314fa492b8b08be10211039012
rodrigollo <rodrigogollo.nh@gmail.com>
2023-08-02 14:03:03 -0300

    fix eslint's

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -4,13 +4,29 @@ module.exports = {
     browser: true,
     es2020: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended', 
+    'plugin:@typescript-eslint/recommended', 
+    'plugin:react-hooks/recommended', 
+    'plugin:storybook/recommended', 
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': ['warn', {
-      allowConstantExport: true
-    }]
+    "@typescript-eslint/type-annotation-spacing": ["warn", {
+      before: false,
+      after: true
+    }],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'indent': 'off',
+    '@typescript-eslint/indent': ['error', 'tab'],
+    'linebreak-style': ['error', 'windows'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'object-curly-spacing': ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'no-multiple-empty-lines': ['error', { 'max': 2, 'maxEOF': 0 }],
   }
 };
